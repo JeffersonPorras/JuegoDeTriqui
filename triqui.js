8ea3b5f59820e4777931e0cba1ad16d1bf98e4aa
Jefferson Porras <jeffersonporras.jp@gmail.com>
2023-02-27 12:41:31 -0500

    verificando arrays para opciones de ganador

--- triqui.js
+++ triqui.js
@@ -1,10 +1,10 @@
 
 var eleccionDelJugador = ''
 var eleccionDelPc = ''
-let celdasJugasdas = []
 let celdasJugadasDelJugador = []
 let celdasJugadasDelPc = []
-
+let celdasJugasdas = []
+celdasJugadasDelJugador.push(celdasJugasdas)
 const opcionesHorizontalesParaGanar = [
     [0,1,2],
     [3,4,5],
@@ -19,13 +19,12 @@ const opcionesDiagonalesParaGanar = [
     [0,4,8],
     [2,4,6],
 ]
-console.log(celdasJugasdas);
 
 /**Esta Funcion me recibe la elecion del jugador con los dos caracteres permitos que son "x" o "o" si el jugador
  *ingresa algun otro caracter mostrara "Debes elegir entre X o O" */
 function eleccionJudador (event,eleccion){
     let resultadoEleccion = ''
-    if(eleccion.toLowerCase() === "x" ){
+    if(eleccion === "x" ){
         resultadoEleccion = "X"
     }else if(eleccion === "o"){
         resultadoEleccion = "O"
@@ -33,7 +32,6 @@ function eleccionJudador (event,eleccion){
         resultadoEleccion = "Debes elegir entre X o O"
     }
     eleccionDelJugador = resultadoEleccion
-    console.log(event);
     event.target.disabled = eleccionDelJugador
     eleccionPc(eleccionDelJugador)
 }
@@ -42,6 +40,8 @@ function eleccionJudador (event,eleccion){
  * esta funcion me retorna "El jugador no ha elejido"
  */
 function eleccionPc (eleccion){
+    let inputO =document.querySelector('input_o')
+    let inputX =document.querySelector('input_x')
     let resultadoEleccionPc = ''
     if(eleccion.toLowerCase() === "x"){
         resultadoEleccionPc = "O"
@@ -51,7 +51,6 @@ function eleccionPc (eleccion){
         resultadoEleccionPc = "El jugador no ha elejido"
     }
     eleccionDelPc = resultadoEleccionPc
-    
     console.log(eleccionDelPc);
 }
 
@@ -78,18 +77,22 @@ function genera_tabla() {
   }
 function anadirInteractividad() {
     let espaciosDeJuego = document.querySelectorAll(".columnaJuego")
-    console.log(espaciosDeJuego);
     espaciosDeJuego.forEach(espacio => {
         espacio.addEventListener('click',siElijioEspacio)
     });
+    event.target.disabled = espaciosDeJuego
 }
 function siElijioEspacio(event) {
     event.target.innerText = eleccionDelJugador
     console.log(event.target);
     celdasJugadasDelJugador.push(event.target.id)
     console.log(eleccionDelJugador);
+    //ElijeElPc(event)
 }
-
+function elijeElPc() {
+    eleccionDelPc = aleatorio(1,8);
+}
+console.log(elijeElPc());
 function imprimirCoordenadas(matrix){
     for(let i = 0; i < matrix.length; i++) {
         for(let j = 0; j < matrix[i].length; j++){
@@ -97,10 +100,22 @@ function imprimirCoordenadas(matrix){
         }
     }
 }
-console.log(imprimirCoordenadas(opcionesHorizontalesParaGanar));
 console.log(celdasJugadasDelJugador);
 
+function ganadorDelJuego(matrix) {
+    let ganadorDelJuego = ''
+    let opcion1 = opcionesHorizontalesParaGanar.find((o) => o === matrix[i][j])
+    let opcion2 = opcionesVerticalesParaGanar.find((o) => o === matrix[i][j])
+    let opcion3 = opcionesDiagonalesParaGanar.find((o) => o === matrix[i][j])//verificar el array coincida jugadas del jugador
+    let jugador1 = celdasJugadasDelJugador
+    if(opcion1 === jugador1 || opcion2 === jugador1 || opcion3 === jugador1){
+        ganadorDelJuego = "Jugador Gana"
+    }else {
+        ganadorDelJuego = "Pc Gana"
+    }
+    return ganadorDelJuego
+}
+console.log(ganadorDelJuego(celdasJugadasDelJugador));
 function aleatorio(min,max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-//opcionesHorizontalesParaGanar.find((o) => o === matrix[i][j])//verificar el array coincida jugadas del jugador
